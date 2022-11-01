d18ffb424a307c95f566d74cc634ce9c599f4295
AlejandraMRuiz <kovydcode@gmail.com>
2022-11-01 21:41:39 +0000

    adds js file I'd created in sandbox for Week 3 homework

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
-app.js;
+let weather = {
+	paris: {
+		temp: 19.7,
+		humidity: 80,
+	},
+	tokyo: {
+		temp: 17.3,
+		humidity: 50,
+	},
+	lisbon: {
+		temp: 30.2,
+		humidity: 20,
+	},
+	sanFrancisco: {
+		temp: 20.9,
+		humidity: 100,
+	},
+	oslo: {
+		temp: -5,
+		humidity: 20,
+	},
+};
+
+let city = prompt('Enter a city');
+
+const cityWeather = weather[city];
+let message;
+
+message = cityWeather
+	? `It is currently ${Math.round(
+			cityWeather.temp
+	  )} in ${city} with a humidity of ${Math.round(cityWeather.humidity)}%`
+	: `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`;
+
+alert(message);
